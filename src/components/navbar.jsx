2d56bf5edf478c7a6b2f87d67e3ef052526bd461
Ryan <rumeleazar@gmail.com>
2021-05-23 20:57:25 +0800

    Added delete database button

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -15,14 +15,33 @@ retrieveData = (e) => {
   });
 
 }
+
+deleteDatabase = (e) => {
+
+  fetch('http://localhost:8000/api/delete', {
+      method: 'DELETE',
+  })
+  .then(response => response.json())
+  .then(data => {
+   this.setState({data:data})
+   console.log(this.state.data)
+  });
+
+}
     render() {
       return (
         <div>
            <nav className ="navbar navbar-light bg-secondary" >
                 <h5 className = "text-light">Currency Conventer</h5>
-                <ul className="navbar-nav list-group-horizontal">
+                <ul className="navbar-nav list-inline">
                   <div className = "p-3">
-                    <button className="btn btn-warning" onClick = {this.retrieveData}>Retrieve Rates</button>
+                    <li className = "list-inline-item">
+                      <button className="btn btn-warning" onClick = {this.retrieveData}>Retrieve Rates</button>
+                    </li>
+                    <li className = "list-inline-item">
+                    <button className="btn btn-danger" onClick = {this.deleteDatabase}>Clear database</button>
+                    </li>
+                    
                   </div>
               </ul>
             </nav>
